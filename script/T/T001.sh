4c4845962921ebd210df4a29a7694313413a1448
hibana2077 <hibana2077@gmail.com>
2025-12-29 08:31:49 +1000

    feat: Add job script T001.sh for GPU resource allocation and execution

---
+++ script/T/T001.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#PBS -P rp06
+#PBS -q gpuvolta
+#PBS -l ngpus=1
+#PBS -l ncpus=12
+#PBS -l mem=32GB
+#PBS -l walltime=38:00:00
+#PBS -l wd
+#PBS -l storage=scratch/rp06
+
+module load cuda/12.6.2
+
+source /scratch/rp06/sl5952/RL-test/.venv/bin/activate
+export HF_HOME="/scratch/rp06/sl5952/RL-test/.cache"
+export HF_HUB_OFFLINE=1
+
+cd ../..
+python3 src/main.py \
+	--config configs/default.yaml \
+	>> T001.log 2>&1
