9fe1bf08ab111745184c530097c751140afa5f0e
hibana2077 <hibana2077@gmail.com>
2025-12-31 18:02:34 +1000

    fix: Update walltime and model parameters in T000.sh for improved training configuration

--- script/T/T000.sh
+++ script/T/T000.sh
@@ -4,7 +4,7 @@
 #PBS -l ngpus=1
 #PBS -l ncpus=12
 #PBS -l mem=32GB
-#PBS -l walltime=38:00:00
+#PBS -l walltime=00:45:00
 #PBS -l wd
 #PBS -l storage=scratch/rp06
 
@@ -15,14 +15,16 @@ export HF_HOME="/scratch/rp06/sl5952/RL-test/.cache"
 export HF_HUB_OFFLINE=1
 
 cd ../..
-python3 src/main.py \
+python3 -u src/main.py \
   --dataset soybean --data_root ./data \
   --val_split train_split --train_val_ratio 0.1 \
-  --model vit_base_patch16_224 --pretrained \
-  --epochs 3 --warmup_epochs 0 --train_last_n 0 \
+  --model vit_base_patch16_dinov3 --pretrained \
+  --epochs 280 --warmup_epochs 200 --train_last_n 0 \
   --lr 1e-3 --lambda_l2 1e-2 \
   --batch_size 32 --num_workers 0 \
-  --augment \
+  --alpha_mode learn --alpha_lr 1e-1 \
+  --alpha_entropy_reg 0.0 --alpha_s_l2_reg 0.0 --alpha_classwise_batch_norm \
   --output_dir ./T000 --run_name T000 \
-  --seed 42
-  >> T000.log 2>&1
+  --img_size 512 \
+  --do_attribution \
+  --seed 42 >> T000.log 2>&1
