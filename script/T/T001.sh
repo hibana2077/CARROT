219aee1f22715bf7c0aa5823ec8fd3261dd28a76
hibana2077 <hibana2077@gmail.com>
2025-12-31 23:30:14 +1000

    fix: Update queue type, CPU count, and walltime in T001.sh for improved resource allocation

--- script/T/T001.sh
+++ script/T/T001.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #PBS -P rp06
-#PBS -q gpuhopper
+#PBS -q dgxa100
 #PBS -l ngpus=1
-#PBS -l ncpus=12
+#PBS -l ncpus=16
 #PBS -l mem=32GB
-#PBS -l walltime=38:00:00
+#PBS -l walltime=00:35:00
 #PBS -l wd
 #PBS -l storage=scratch/rp06
 
@@ -15,15 +15,16 @@ export HF_HOME="/scratch/rp06/sl5952/RL-test/.cache"
 export HF_HUB_OFFLINE=1
 
 cd ../..
-python3 src/main.py \
+python3 -u src/main.py \
   --dataset soybean --data_root ./data \
   --val_split train_split --train_val_ratio 0.1 \
-  --model vit_base_patch16_224 --pretrained \
-  --epochs 3 --warmup_epochs 0 --train_last_n 0 \
+  --model vit_base_patch16_dinov3 --pretrained \
+  --epochs 280 --warmup_epochs 200 --train_last_n 0 \
   --lr 1e-3 --lambda_l2 1e-2 \
   --batch_size 32 --num_workers 0 \
-  --do_attribution --attribution_limit 20 --top_k 5 \
-  --similarity cosine --attribution_class pred \
-  --output_dir ./T001 --run_name T001 \
-  --seed 42
-  >> T001.log 2>&1
+  --alpha_mode learn --alpha_lr 1e-1 \
+  --alpha_entropy_reg 0.0 --alpha_s_l2_reg 0.0 --alpha_classwise_batch_norm \
+  --output_dir ./T010 --run_name T010 \
+  --img_size 512 \
+  --do_attribution \
+  --seed 42 >> T010.log 2>&1
