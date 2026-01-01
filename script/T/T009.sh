759ceeddeff40d3446573ffe79aad1ff9892ca9c
hibana2077 <hibana2077@gmail.com>
2026-01-02 00:10:28 +1000

    feat: Add job script T009.sh for training setup and execution

---
+++ script/T/T009.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#PBS -P yp87
+#PBS -q gpuhopper
+#PBS -l ngpus=1
+#PBS -l ncpus=12
+#PBS -l mem=16GB
+#PBS -l walltime=02:45:00
+#PBS -l wd
+#PBS -l storage=scratch/yp87
+
+module load cuda/12.6.2
+
+source /scratch/yp87/sl5952/CARROT/.venv/bin/activate
+export HF_HOME="/scratch/yp87/sl5952/CARROT/.cache"
+export HF_HUB_OFFLINE=1
+
+cd ../..
+python3 -u src/main.py \
+  --dataset cub_200_2011 --data_root ./data \
+  --model efficientnet_x_b5.sw_r448_e450_in1k --pretrained \
+  --epochs 280 \
+  --lr 1e-3 \
+  --batch_size 64 --num_workers 0 \
+  --img_size 448 \
+  --seed 42 >> T009.log 2>&1
