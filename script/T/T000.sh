eefae328b953ffac08ca5f6ddc60dc24a626bce4
hibana2077 <hibana2077@gmail.com>
2026-01-01 20:47:13 +1000

    fix: Update memory allocation and storage path in T000.sh for correct resource management

--- script/T/T000.sh
+++ script/T/T000.sh
@@ -1,30 +1,26 @@
 #!/bin/bash
-#PBS -P kf09
+#PBS -P yp87
 #PBS -q gpuvolta
 #PBS -l ngpus=1
 #PBS -l ncpus=12
-#PBS -l mem=32GB
+#PBS -l mem=16GB
 #PBS -l walltime=02:45:00
 #PBS -l wd
-#PBS -l storage=scratch/rp06
+#PBS -l storage=scratch/yp87
 
 module load cuda/12.6.2
 
-source /scratch/rp06/sl5952/RL-test/.venv/bin/activate
-export HF_HOME="/scratch/rp06/sl5952/RL-test/.cache"
+source /scratch/yp87/sl5952/CARROT/.venv/bin/activate
+export HF_HOME="/scratch/yp87/sl5952/CARROT/.cache"
 export HF_HUB_OFFLINE=1
 
 cd ../..
 python3 -u src/main.py \
   --dataset soybean --data_root ./data \
-  --val_split train_split --train_val_ratio 0.1 \
   --model vit_base_patch16_dinov3 --pretrained \
-  --epochs 280 --warmup_epochs 200 --train_last_n 0 \
-  --lr 1e-3 --lambda_l2 1e-2 \
+  --epochs 280 \
+  --lr 1e-3 \
   --batch_size 32 --num_workers 0 \
-  --alpha_mode learn --alpha_lr 1e-1 \
-  --alpha_entropy_reg 0.0 --alpha_s_l2_reg 0.0 --alpha_classwise_batch_norm \
   --output_dir ./T000 --run_name T000 \
   --img_size 512 \
-  --do_attribution \
   --seed 42 >> T000.log 2>&1
