f1ea95aa9a2bfabd8c5e4062c7e637e856a5263b
hibana2077 <hibana2077@gmail.com>
2026-01-02 15:17:00 +1000

    feat: update dataset in D000.sh from cotton80 to soybean for consistency in training

--- script/D/D000.sh
+++ script/D/D000.sh
@@ -16,7 +16,7 @@ export HF_HUB_OFFLINE=1
 
 cd ../..
 python3 -u src/main.py \
-  --dataset cotton80 --data_root ./data \
+  --dataset soybean --data_root ./data \
   --model vit_base_patch16_dinov3 --pretrained \
   --epochs 280 \
   --batch_size 256 --num_workers 0 \
