b0057132269a063c0ac4eac4cd0fdf27013709dc
hibana2077 <hibana2077@gmail.com>
2026-01-02 11:24:10 +1000

    feat: add job submission scripts C000.sh, C001.sh, C002.sh, C003.sh, C004.sh with varying lambda values; update report.csv with new job configurations

---
+++ script/C/C000.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#PBS -P yp87
+#PBS -q gpuvolta
+#PBS -l ngpus=1
+#PBS -l ncpus=12
+#PBS -l mem=16GB
+#PBS -l walltime=12:45:00
+#PBS -l wd
+#PBS -l storage=scratch/yp87
+
+module load cuda/12.6.2
+
+source /scratch/yp87/sl5952/CARROT/.venv/bin/activate
+export HF_HOME="/scratch/yp87/sl5952/CARROT/.cache"
+export HF_HUB_OFFLINE=1
+
+cd ../..
+python3 -u src/main.py \
+  --dataset cotton80 --data_root ./data \
+  --model vit_base_patch16_dinov3 --pretrained \
+  --epochs 280 \
+  --batch_size 16 --num_workers 0 \
+  --img_size 288 \
+  --use_carrot \
+  --carrot_lambda 0.02 --carrot_alpha 10 --carrot_topm 20 --carrot_conf_topk 20 --carrot_warmup_epochs 10 \
+  --seed 42 >> C000.log 2>&1
+
+
+# lambda ∈ {0.02, 0.05, 0.1, 0.2, 0.4}
