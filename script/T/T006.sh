6d86bf28dd5e659cafac9dce876538dd0e5d422b
hibana2077 <hibana2077@gmail.com>
2026-01-03 16:24:09 +1000

    feat: Add T006.sh script for model training with CARROT parameters

---
+++ script/T/T006.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#PBS -P yp87
+#PBS -q gpuhopper
+#PBS -l ngpus=1
+#PBS -l ncpus=12
+#PBS -l mem=8GB
+#PBS -l walltime=08:00:00
+#PBS -l wd
+#PBS -l storage=scratch/yp87
+
+module load cuda/12.6.2
+
+source /scratch/yp87/sl5952/CARROT/.venv/bin/activate
+export HF_HOME="/scratch/yp87/sl5952/CARROT/.cache"
+export HF_HUB_OFFLINE=1
+
+cd ../..
+python3 -u src/main.py \
+  --dataset cub_200_2011 --data_root ./data \
+  --model swinv2_tiny_window16_256.ms_in1k --pretrained \
+  --epochs 1000 \
+  --batch_size 256 --num_workers 0 \
+  --img_size 256 \
+  --carrot --carrot_topk 5 --carrot_gamma 0.0 --carrot_delta 0.1 --carrot_aug_per_sample 2 --carrot_aug_weight 0.5 \
+  --carrot_warmup_epochs 200 \
+  --seed 42 >> T006.log 2>&1
