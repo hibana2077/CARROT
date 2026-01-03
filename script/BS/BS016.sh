e2d057f7c26840231a2195094efc159df650954f
hibana2077 <hibana2077@gmail.com>
2026-01-03 10:20:53 +1000

    feat: add job submission scripts BS015.sh, BS016.sh, BS017.sh, BS018.sh, and BS019.sh for model training with varying batch sizes and configurations

---
+++ script/BS/BS016.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#PBS -P yp87
+#PBS -q dgxa100
+#PBS -l ngpus=1
+#PBS -l ncpus=16
+#PBS -l mem=14GB
+#PBS -l walltime=05:45:00
+#PBS -l wd
+#PBS -l storage=scratch/yp87
+
+module load cuda/12.6.2
+
+source /scratch/yp87/sl5952/CARROT/.venv/bin/activate
+export HF_HOME="/scratch/yp87/sl5952/CARROT/.cache"
+export HF_HUB_OFFLINE=1
+
+cd ../..
+python3 -u src/main.py \
+  --dataset cub_200_2011 --data_root ./data \
+  --model densenet121.ra_in1k --pretrained \
+  --epochs 280 \
+  --batch_size 32 --num_workers 0 \
+  --img_size 256 \
+  --seed 42 >> BS016.log 2>&1
