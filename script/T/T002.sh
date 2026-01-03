53d61c89f90e598a1b58b1fe88de09bf225f45f0
hibana2077 <hibana2077@gmail.com>
2026-01-04 00:20:53 +1000

    feat: Add T002.sh and T003.sh scripts for model training with updated configurations

---
+++ script/T/T002.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#PBS -P yp87
+#PBS -q gpuhopper
+#PBS -l ngpus=1
+#PBS -l ncpus=12
+#PBS -l mem=8GB
+#PBS -l walltime=08:00:00
+#PBS -l wd
+#PBS -l storage=scratch/yp87
+
+module load cuda/12.6.2
+
+source /scratch/yp87/sl5952/CARROT/.venv/bin/activate
+export HF_HOME="/scratch/yp87/sl5952/CARROT/.cache"
+export HF_HUB_OFFLINE=1
+
+cd ../..
+python3 -u src/main.py \
+  --dataset cotton80 --data_root ./data \
+  --model vit_base_patch8_224.augreg2_in21k_ft_in1k --pretrained \
+  --epochs 1000 \
+  --batch_size 256 --num_workers 0 \
+  --img_size 224 \
+  --head linear \
+  --seed 42 >> T002.log 2>&1
