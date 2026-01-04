7df7eeb767b628b9abf8c203e5c8d68c8ca9d8ae
hibana2077 <hibana2077@gmail.com>
2026-01-04 20:47:54 +1000

    feat: Add job scripts C000.sh and C001.sh for training with specified parameters and resources

---
+++ script/C/C000.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#PBS -P yp87
+#PBS -q gpuhopper
+#PBS -l ngpus=1
+#PBS -l ncpus=12
+#PBS -l mem=8GB
+#PBS -l walltime=08:00:00
+#PBS -l wd
+#PBS -l storage=scratch/yp87
+
+module load cuda/12.6.2
+
+source /scratch/yp87/sl5952/CARROT/.venv/bin/activate
+export HF_HOME="/scratch/yp87/sl5952/CARROT/.cache"
+export HF_HUB_OFFLINE=1
+
+cd ../..
+python3 -u src/main.py \
+  --dataset cub_200_2011 --data_root ./data \
+  --model tinynet_a.in1k --pretrained \
+  --epochs 1000 \
+  --batch_size 48 --num_workers 0 \
+  --img_size 256 \
+  --optimizer adamw \
+  --lr 0.0005 \
+  --seed 42 >> C000.log 2>&1
